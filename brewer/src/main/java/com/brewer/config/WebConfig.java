32464eb5c7b31f061a63b55890a8515b3aaece77
Lab2 <Lab2@Lab1-Professor>
2017-06-26 16:33:33 -0300

    Projeto configurado e inicializado.

---
+++ brewer/src/main/java/com/brewer/config/WebConfig.java
@@ -0,0 +1,70 @@
+package com.brewer.config;
+
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.templateresource.SpringResourceTemplateResource;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import com.brewer.controller.CervejasController;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = {CervejasController.class})
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+	private ApplicationContext applicationContext;	
+	// essa classe tem 3 objetos para o funcionamento das views
+	
+	private ITemplateResolver templateResolver(){
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/"); // onde estão os templates
+		resolver.setSuffix(".html"); // termino de arquivo
+		resolver.setTemplateMode(TemplateMode.HTML); // modo de template - HTML
+		return resolver;
+	}
+	
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+			this.applicationContext = applicationContext;
+	}
+	
+	@Bean
+	public TemplateEngine templateEngine(){
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver(){
+		ThymeleafViewResolver resolver=  new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+	
+	
+		
+	
+	
+	
+	
+	
+	
+}	
