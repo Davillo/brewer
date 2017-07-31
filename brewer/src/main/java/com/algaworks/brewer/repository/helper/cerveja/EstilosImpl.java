34686f05224949a7cf8ceb631ff8776ca170912c
Davillo <davilloaurelio@hotmail.com>
2017-07-31 16:31:36 -0300

    bean util para paginação

--- brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/EstilosImpl.java
+++ brewer/src/main/java/com/algaworks/brewer/repository/helper/cerveja/EstilosImpl.java
@@ -9,6 +9,7 @@ import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +20,7 @@ import org.springframework.util.StringUtils;
 import com.algaworks.brewer.model.Cerveja;
 import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.repository.filter.EstiloFilter;
+import com.algaworks.brewer.repository.paginacao.PaginacaoUtil;
 
 public class EstilosImpl implements EstilosQueries {
 
@@ -26,25 +28,16 @@ public class EstilosImpl implements EstilosQueries {
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Autowired
+	private PaginacaoUtil paginacaoUtil;
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly=true)
 	public Page<Estilo> filtrar(EstiloFilter filtro, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Estilo.class);
-		int totalRegistrosPagina = pageable.getPageSize();
-		int paginaAtual = pageable.getPageNumber();
-		int primeiroRegistro = paginaAtual * totalRegistrosPagina;
 		
-		criteria.setFirstResult(primeiroRegistro);
-		criteria.setMaxResults(totalRegistrosPagina);
-		
-		Sort sort = pageable.getSort();
-		if(sort != null){
-			Sort.Order order = sort.iterator().next();
-			String property = order.getProperty();
-			criteria.addOrder(order.isAscending() ? Order.asc(property) : Order.desc(property));
-		}
-		
+		paginacaoUtil.preparar(criteria, pageable);
 		adicionarFiltro(filtro, criteria);
 			
 			
