bc68220c1ab21f0b49e5c95de353f73d82266c57
Davillo <davilloaurelio@hotmail.com>
2017-09-17 17:42:10 -0300

    Nome do cliente selecionado na pesquisa rápida

--- brewer/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
+++ brewer/src/main/java/com/algaworks/brewer/security/AppUserDetailsService.java
@@ -1,14 +1,9 @@
 package com.algaworks.brewer.security;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,7 +24,8 @@ public class AppUserDetailsService implements UserDetailsService {
 		Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha incorretos"));
 		return new UsuarioSistema(usuario, new HashSet<>());
 	}
-
+	
+/*
 	private Collection<? extends GrantedAuthority> getPermissoes(Usuario usuario) {
 		Set<SimpleGrantedAuthority> authorities = new HashSet<>();
 		
@@ -38,5 +34,5 @@ public class AppUserDetailsService implements UserDetailsService {
 		
 		return authorities;
 	}
-
+*/
 }
